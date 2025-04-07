9f751c0ecbcf24df6d793eab1e3528cc85acf53e
이승규 <65271132+Leeseunggyu3@users.noreply.github.com>
2025-04-07 16:43:35 +0900

    [Bronze I] Title: 세로읽기, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ C++17 (Clang)/백준/Bronze/10798. 세로읽기/세로읽기.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string arr[5];
+
+    for (int i = 0; i < 5; i++) {
+        cin >> arr[i];
+    }
+
+    for (int j = 0; j < 15; j++) {
+        for (int i = 0; i < 5; i++) {
+            if (j < arr[i].length()) {
+                cout << arr[i][j];
+            }
+        }
+    }
+
+    return 0;
+}
